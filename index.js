b59de1f82ea87c2abe84cc017ca5e3394a6a0dac
Claire <clairelogan16@gmail.com>
2021-06-29 17:32:10 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(name => name.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(name => name.slice(0,1).toUpperCase() === string.slice(0,1).toUpperCase());
+}
+
+function matchName(drivers, string) {
+    return drivers.filter( element => element.name.toUpperCase() === string.toUpperCase());
+}
+
