dd75befe7cc5b63f7f2d6ecf6a9309b6d20586ba
Claire <clairelogan16@gmail.com>
2021-07-05 16:03:32 -0700

    commit tree bb3ac7a0e61b82870b7870be476a37636f7045de

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 
-function findMatching(drivers, string) {
-    return drivers.filter(name => name.toUpperCase() === string.toUpperCase());
-}
+// function findMatching(drivers, string) {
+//     return drivers.filter(name => name.toUpperCase() === string.toUpperCase());
+// }
+
+// function fuzzyMatch(drivers, string) {
+//     return drivers.filter(name => name.slice(0,1).toUpperCase() === string.slice(0,1).toUpperCase());
+// }
+
+// function matchName(drivers, string) {
+//     return drivers.filter( element => element.name.toUpperCase() === string.toUpperCase());
+// }
+
+
+
+//alternate tries
+
+// function findMatching(drivers, name){
+//     const matchDrivers = drivers.filter(driver => {
+//         return driver.toUpperCase() === name.toUpperCase();
+//     })
+
+//     return matchDrivers
+// }
+
+
+// function fuzzyMatch(drivers, letter){
+//     const fuzzyDrivers = drivers.filter(driver => {
+//         return driver.slice(0,1).toUpperCase() === letter.slice(0,1).toUpperCase();
+//     })
+
+//     return fuzzyDrivers;
+// }
+
+
+// function matchName(drivers, string){
+//     const matchNames = drivers.filter(driver => {
+//         return driver.name === string;
+//     })
+
+//     return matchNames;
+
+// }
+
+
+//practice 7/5
+
+function findMatching(drivers, string){
+   return drivers.filter(driver => driver.toUpperCase() === string.toUpperCase())
+};
 
 function fuzzyMatch(drivers, string) {
-    return drivers.filter(name => name.slice(0,1).toUpperCase() === string.slice(0,1).toUpperCase());
+    return drivers.filter(driver => driver[0].toUpperCase() === string[0].toUpperCase())
 }
 
-function matchName(drivers, string) {
-    return drivers.filter( element => element.name.toUpperCase() === string.toUpperCase());
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name.toUpperCase() === string.toUpperCase())
 }
-
